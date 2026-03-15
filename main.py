851eb3a2caa6f1f83ceabde503abe25dc1922fee
danielfurman03369 <danielfurman03@gmail.com>
2026-03-15 17:42:04 +0200

    first

---
+++ main.py
@@ -0,0 +1,27 @@
+# gr = {
+#     1: 2,
+#     'a': 3,
+#     3: 5
+# }
+# print(gr['a'] < (gr[3]))
+from pprint import pprint
+# my_dict = {
+#     'name': 'daniel',
+#     'year_of_birth': 2003,
+#     'height': '181 cm',
+#     'phone': '0554532635',
+#     'sibilings': ["alina", "gil"],
+#     'address' : {
+#         'city': 'qadima',
+#         'house': 1,
+#         'street': 'hanafa'
+#     },
+# }
+# for key, value in my_dict.items():
+#     print(key,':', value)
+# pprint(my_dict)
+# print(my_dict['sibilings'][0])
+# print(2026 - my_dict['year_of_birth'])
+
+
+
